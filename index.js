6f9a10a2f5b07232b825744e820f972e25eb0847
Whiting <whitinghtennis@gmail.com>
2021-06-30 14:42:43 -0700

    commit tree 80ffa43b7597bd8fc7ff6644e315395daf901cc9

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+
+
+function findMatching(array, driverName) {
+   return array.filter(function (driver) { return driver.toUpperCase() === driverName.toUpperCase(); })
+}
+
+function fuzzyMatch(array, driverName) {
+    return array.filter(function (driver) {return driver.charAt(0) === driverName.charAt(0)})
+  }
+
+  function matchName(array, driverName) {
+    return array.filter(function (driver) { return driver.name === driverName; })
+ }    
